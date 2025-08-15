2934c8fd21079c0f8d5c0aa612c2f65e202ba69e
OriC28 <orianacolina.perea@gmail.com>
2025-08-15 15:31:33 -0400

    Add form validations on the client side

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,18 +1,25 @@
 import { createOpinion } from "./api.js";
+import { validateId, validateUsername, validateMessage } from "./validation.js";
 
 const form = document.getElementById("form-id");
 
 const handleForm = async (e) => {
     e.preventDefault();
     const {id, username, message} = e.target;
-
     const data = {
         image_id: id.value,
         username: username.value,
         message: message.value
     };
-    const response = await createOpinion(data);
-    console.log(response);
+    const idResult = validateId(id.value);
+    const usernameResult = validateUsername(username.value);
+    const messageResult = validateMessage(message.value);
+
+    if(idResult.isValid && usernameResult.isValid && messageResult.isValid){
+        const response = await createOpinion(data);
+        console.log(response);
+    }
+   
     console.log(id.value, username.value, message.value);
 }
 
--- frontend/src/js/validation.js
+++ frontend/src/js/validation.js
@@ -1,12 +1,44 @@
 
 export const validateId = (id) => {
-
+    if(validator.isEmpty(id)){
+        return {isValid: false, message: "El id es requerido."};
+    }
+    if(!validator.isInt(id, {min: 1})){
+        return {isValid: false, message: "El id debe ser un entero mayor a 0."};
+    }
+    return {isValid: true};
 }
 
 export const validateUsername = (username) => {
+    if(validator.isEmpty(username)){
+        return {isValid: false, message: "El nombre de usuario es requerido."};
+    }
+    if(validator.matches(username, /^[0-9.+-]+$/g)){
+        return {isValid: false, message: "El nombre de usuario no puede ser numérico."};
+    }
+    if(!validator.isLength(username, {min: 3, max: 20})){
+        return {isValid: false, message: "El nombre de usuario debe tener entre 3 y 20 caracteres."};
+    }
 
+    if(!validator.matches(username, /^[a-zA-Z0-9._-]/)){
+        return {isValid: false, message: "El nombre de usuario contiene caracteres invalidos."};
+    }
+    return {isValid: true};
 }
 
 export const validateMessage = (msg) => {
+     if(validator.isEmpty(msg)){
+        return {isValid: false, message: "El mensaje es requerido."};
+    }
+    if(validator.matches(msg, /^[0-9.+-]+$/g)){
+        return {isValid: false, message: "El mensaje no puede ser numérico."};
+    }
+    if(!validator.isLength(msg, {min: 2, max: 255})){
+        return {isValid: false, message: "El mensaje debe tener entre 3 y 20 caracteres."};
+    }
 
+    if(!validator.matches(msg, /^(?!\s*$).{2,255}$/)){
+        return {isValid: false, message: "El mensaje contiene caracteres invalidos."};
+    }
+    return {isValid: true};
 }
